01bd15375860cfc4b8e925fc2d935378ee89d3c5
htuanqn <phamhoangtuanqn@gmail.com>
2026-09-22 19:43:27 +0700

    feat: auth config

---
+++ backend/src/main/java/com/techx/intervue/config/AuthConfig.java
@@ -0,0 +1,21 @@
+package com.techx.intervue.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Getter
+public class AuthConfig {
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private Long expirationTime;
+
+    @Value("${jwt.issuer}")
+    private String issuer;
+
+    @Value("${jwt.refresh-token-ttl-days}")
+    private Integer refreshTokenTTLDays = 14;
+}
