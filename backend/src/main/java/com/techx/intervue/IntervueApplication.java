90f60abf5f35a35c213c7c6c6e59b5d9bd16dbfa
htuanqn <phamhoangtuanqn@gmail.com>
2026-09-22 18:11:43 +0700

    feat: init project backend

---
+++ backend/src/main/java/com/techx/intervue/IntervueApplication.java
@@ -0,0 +1,13 @@
+package com.techx.intervue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntervueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntervueApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/techx/intervue/IntervueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techx.intervue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntervueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
