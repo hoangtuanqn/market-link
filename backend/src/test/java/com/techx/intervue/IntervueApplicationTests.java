cfb7af159c1f6f21b7b67e24115eaf901154cde1
htuanqn <phamhoangtuanqn@gmail.com>
2026-09-22 18:58:37 +0700

    style: format java files with spotless

--- backend/src/test/java/com/techx/intervue/IntervueApplicationTests.java
+++ backend/src/test/java/com/techx/intervue/IntervueApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class IntervueApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {}
 }
