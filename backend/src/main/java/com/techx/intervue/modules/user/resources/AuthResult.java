2ba57b16ebe4eaee249e1e839f0e26fb2766cca9
htuanqn <phamhoangtuanqn@gmail.com>
2026-09-25 01:28:35 +0700

    feat: accept remember me on login

--- backend/src/main/java/com/techx/intervue/modules/user/resources/AuthResult.java
+++ backend/src/main/java/com/techx/intervue/modules/user/resources/AuthResult.java
@@ -1,3 +1,5 @@
 package com.techx.intervue.modules.user.resources;
 
-public record AuthResult(String accessToken, String refreshToken, UserResource user) {}
+/** rememberMe quyết định cookie refresh_token là cookie phiên hay cookie lâu dài. */
+public record AuthResult(
+        String accessToken, String refreshToken, UserResource user, boolean rememberMe) {}
