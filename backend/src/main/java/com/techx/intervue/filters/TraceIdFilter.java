9c5a9d9ad41499d3e31a3c71ad69fe9b5f2833e5
htuanqn <phamhoangtuanqn@gmail.com>
2026-09-24 07:46:24 +0700

    feat: add trace id filter

---
+++ backend/src/main/java/com/techx/intervue/filters/TraceIdFilter.java
@@ -0,0 +1,31 @@
+package com.techx.intervue.filters;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+import org.slf4j.MDC;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Order(2) // cho nó chạy sớm nhất, vì lỗi nào cũng cần Trace Id
+@Component
+public class TraceIdFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String traceId = UUID.randomUUID().toString();
+        MDC.put("traceId", traceId);
+        response.setHeader("X-Trace-Id", traceId);
+        try {
+            filterChain.doFilter(request, response);
+        } finally {
+            // vẫn chạy nếu trong try bị throw
+            MDC.clear();
+        }
+    }
+}
