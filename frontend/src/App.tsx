d78fab7deb6a17c25145fdabd7508fa5b65f17fc
ANDEV09 <lamhoangan612@gmail.com>
2026-09-25 14:04:42 +0700

    feat: add farmer stall profile page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -41,6 +41,7 @@ import FarmerOrderDetailPage from './pages/FarmerOrderDetail';
 import FarmerStockWeekPage from './pages/FarmerStockWeek';
 import FarmerProductsPage from './pages/FarmerProducts';
 import FarmerProductFormPage from './pages/FarmerProductForm';
+import FarmerStallProfilePage from './pages/FarmerStallProfile';
 import AdminLoginPage from './pages/AdminLogin';
 import AdminHomePage from './pages/AdminHome';
 import AdminVerifyPage from './pages/AdminVerify';
@@ -158,6 +159,7 @@ const App = () => {
               </RemountOnParam>
             }
           />
+          <Route path="stall" element={<FarmerStallProfilePage />} />
         </Route>
 
         {/* FR-004: khu admin tách khỏi layout Customer/Farmer. */}
